26c8dee83d1cbba9a7fa4b638b25d871a0164038
Arghyadeep Majumder <arghya@skaas.guru>
2020-09-15 18:33:55 +0530

    Fix: API url changed

--- src/services/Axios/index.js
+++ src/services/Axios/index.js
@@ -2,15 +2,15 @@ import { getAirports } from '../Redux/actions';
 import axios from 'axios';
 
 function aiportSearch() {
-    return (dispatch) => {
-      axios.get('https://argodeep.github.io/React-Redux-Hooks-with-axios-redux-thunk/airport.json')
-        .then(response => {
-          dispatch(getAirports(response.data))
-        })
-        .catch(error => {
-            throw(error);
-        });
-    };
+  return (dispatch) => {
+    axios.get(window.location.origin + '/' + window.location.pathname.split('/')[1] + '/airport.json')
+      .then(response => {
+        dispatch(getAirports(response.data))
+      })
+      .catch(error => {
+        throw (error);
+      });
+  };
 }
 
 export { aiportSearch }
